6309a7174121e4da219f3f1f4e622e79eec3f87a
daynearron <daynearron@gmail.com>
2017-04-09 17:38:39 -0700

    more

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 $(document).foundation();
 $(function(){
 	//Animsition
-	$(".animsition").animsition({
+	$(".row").animsition({
 		inClass: 'fade-in-up-sm',
 		outClass: 'fade-out-down-sm',
+		inDuration: 500,
+		outDuration: 500,
+		linkElement: '.animsition-link',
+		// e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+		loading: true,
+		loadingParentElement: 'body', //animsition wrapper element
+		loadingClass: 'animsition-loading',
+		unSupportCss: [
+		  'animation-duration',
+		  '-webkit-animation-duration',
+		  '-o-animation-duration'
+		]
+	});
+	$(".hero").animsition({
+		inClass: 'fade-in-down-sm',
+		outClass: 'fade-out-up-sm',
 		inDuration: 500,
 		outDuration: 500,
 		linkElement: '.animsition-link',
